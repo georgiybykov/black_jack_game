ef20ed51f9cae9d5ad9afd33ef3e86000f7dc01e
georgiybykov <zhora.bykov@gmail.com>
2020-03-26 16:35:09 +0300

    created hand.rb to set hands to dealer && player

---
+++ hand.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative 'card'
+
+class Hand
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def <<(card)
+    if card.value == 'Ace'
+      @cards << card
+    else
+      @cards.unshift(card)
+    end
+    @cards
+  end
+
+  def show
+    @cards.map(&:show)
+  end
+
+  def score
+    # reduce() is alias method to inject()
+    @cards.reduce(0) do |sum, card|
+      scr = if %w[Jack Queen King].include?(card.value)
+              10
+            elsif card.value == 'Ace'
+              sum + 11 >= 21 ? 1 : 11
+            else
+              card.value
+            end
+      sum + scr
+    end
+  end
+end

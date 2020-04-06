f8440bfa253fbe5694ce77f4037aca87c602912c
georgiybykov <zhora.bykov@gmail.com>
2020-04-06 05:08:27 +0300

    exceptions inheritance syntax fix by ruby 2.7.0-core

--- lib/my_exceptions_library.rb
+++ lib/my_exceptions_library.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MyExceptionsLibrary
-  class NoMoneyError < StandardError
+  class NoMoneyError < ::StandardError
   end
 
   class PlayerNoMoneyError < NoMoneyError
